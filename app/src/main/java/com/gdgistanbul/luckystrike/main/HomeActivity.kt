fab7d45f3243306ed30bc6ef401f01b30644329e
mertsimsek <mertsimsek1992@gmail.com>
2018-02-21 22:44:09 +0300

    Closes #8

--- app/src/main/java/com/gdgistanbul/luckystrike/main/HomeActivity.kt
+++ app/src/main/java/com/gdgistanbul/luckystrike/main/HomeActivity.kt
@@ -5,8 +5,17 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.gdgistanbul.luckystrike.R
+import com.gdgistanbul.luckystrike.model.Constant
+import com.gdgistanbul.luckystrike.model.User
+import com.gdgistanbul.util.PrefUtil
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class HomeActivity : AppCompatActivity() {
+
+    private val databaseReference = FirebaseDatabase.getInstance().reference
 
     companion object {
         fun newIntent(context: Context) = Intent(context, HomeActivity::class.java)
@@ -23,5 +32,88 @@ class HomeActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
                 .add(R.id.container, WaitingFragment.newInstance())
                 .commit()
+
+
+        /**
+         * The listener receives a DataSnapshot that contains the data at the specified location in the database at the time of the event.
+         * Calling getValue() on a snapshot returns the Java object representation of the data.
+         * If no data exists at the location, calling getValue() returns null.
+         */
+        databaseReference.child(Constant.LOOSERS)
+                .addValueEventListener(object : ValueEventListener {
+                    override fun onCancelled(p0: DatabaseError?) {
+                    }
+
+                    override fun onDataChange(dataSnapshot: DataSnapshot?) {
+
+                        dataSnapshot?.let {
+                            dataSnapshot.children
+                                    .map { it.getValue(User::class.java) }
+                                    .forEach {
+                                        it?.let {
+                                            if (isSameUser(it.userId)) {
+                                                openLoserPage()
+                                            }
+                                        }
+                                    }
+                        }
+                    }
+                })
+
+        /**
+         * The listener receives a DataSnapshot that contains the data at the specified location in the database at the time of the event.
+         * Calling getValue() on a snapshot returns the Java object representation of the data.
+         * If no data exists at the location, calling getValue() returns null.
+         */
+        databaseReference.child(Constant.PICKER)
+                .addValueEventListener(object : ValueEventListener {
+                    override fun onCancelled(p0: DatabaseError?) {
+                    }
+
+                    override fun onDataChange(dataSnapshot: DataSnapshot?) {
+
+                        dataSnapshot?.let {
+
+                            dataSnapshot.children
+                                    .map { it.getValue(User::class.java) }
+                                    .forEach {
+                                        it?.let {
+                                            if (isSameUser(it.userId)) {
+                                                openPickerPage()
+                                            }
+                                        }
+                                    }
+                        }
+                    }
+                })
+    }
+
+    /**
+     * get member id from local data source.
+     */
+    private fun getMemberId(): String {
+        return PrefUtil.getString(this, Constant.USER_ID, "")
+    }
+
+    /**
+     * Skip to picker page.
+     */
+    private fun openPickerPage() {
+        supportFragmentManager.beginTransaction()
+                .add(R.id.container, PickerFragment.newInstance())
+                .commit()
+    }
+
+    /**
+     * Skip to loser page.
+     */
+    private fun openLoserPage() {
+        supportFragmentManager.beginTransaction()
+                .add(R.id.container, LostFragment.newInstance())
+                .commit()
+    }
+
+    private fun isSameUser(userId: String): Boolean {
+        return userId.equals(getMemberId())
     }
 }
--- app/src/main/java/com/gdgistanbul/luckystrike/main/LostFragment.kt
+++ app/src/main/java/com/gdgistanbul/luckystrike/main/LostFragment.kt
@@ -1,6 +1,20 @@
 package com.gdgistanbul.luckystrike.main
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gdgistanbul.luckystrike.R
 
 class LostFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = LostFragment()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val view = inflater?.inflate(R.layout.fragment_lost, container, false)
+        return view?.rootView!!
+    }
 }
